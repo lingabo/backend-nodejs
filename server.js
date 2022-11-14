5ac4b91c08dc49e6843bb7f3e73e624c81a09f1d
lingabo <lingabojunior@gmail.com>
2022-11-14 15:50:46 +0100

    read automatically with nodemon

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("voila le serveur a bien demarrer");
+  res.end("voila le serveur a bien demarrer ");
 });
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3300);
